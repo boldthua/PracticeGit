d064bd065926cfd7f9b091190d2a53d915f45d17
周政輝 <leo604leo604@gmail.com>
2024-11-07 11:40:12 +0800

    建立UserModel

--- GitPractice/Models/UserModel.cs
+++ GitPractice/Models/UserModel.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace GitPractice.Models
 {
     public class UserModel
     {
-        
+        public String Name { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+        public String Address { get; set; }
+        public String Phone { get; set; }
     }
 }
