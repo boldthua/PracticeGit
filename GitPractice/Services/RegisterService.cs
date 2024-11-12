20671b0180816817497aa14b424aa8cf1285d15f
周政輝 <leo604leo604@gmail.com>
2024-11-12 09:30:03 +0800

    完成V1版本 RegisterService

--- GitPractice/Services/RegisterService.cs
+++ GitPractice/Services/RegisterService.cs
@@ -1,7 +1,20 @@
+using System;
+using GitPractice.Models;
+
 namespace GitPractice.Services
 {
     public class RegisterService
     {
-        
+        public bool Regist(UserModel model)
+        {
+            if (model.Account.Equals("leo604leo604@gmail.com"))
+            {
+                Console.WriteLine("帳號已存在，請重新輸入帳號");
+                return false;
+            }
+
+            return true;
+        }
+
     }
 }
